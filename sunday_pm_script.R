98cbe9eb2eeec6048201cb0a8a21918ffade6c73
Goutam Chandra Sarker <Goutam.Sarker.DCC@gmail.com>
2015-04-19 14:01:12 -0500

    playing with functions.

--- sunday_pm_script.R
+++ sunday_pm_script.R
@@ -1,2 +1,16 @@
 # This is a ...............
 #
+
+
+# read in the data as tab-delimited
+gap.in <- read.table("output/combined_gapMinder.tsv", sep= "\t", header=TRUE)
+
+for (row.number in 1:10) {
+  for (col.number in 1:5){
+    print(gap.in[row.number,col.number])
+  }
+}
+ gap.in[1:10,1:5]                     
+add.me <- function(x,y){
+  (x+y)*8
+}
