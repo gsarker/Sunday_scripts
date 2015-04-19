682b070095505c6af892973f7abc39e964f06eef
Goutam Chandra Sarker <Goutam.Sarker.DCC@gmail.com>
2015-04-19 15:01:07 -0500

    data tidyer..

--- sunday_pm_script.R
+++ sunday_pm_script.R
@@ -22,3 +22,33 @@ for (row.number in 1:10) {
 add.me <- function(x,y){
   (x+y)*8
 }
+
+
+gap.in %>%
+  filter(country == "China") %>%
+  select(country,year,pop) %>%
+  group_by(country) %>%
+  summarize(min = min(pop))
+
+
+# Challenge
+gap.in %>%
+  filter (year < 1990) %>%
+  group_by(continent, year) %>%
+  
+  select (continent,year,pop) %>%
+  
+  summarize(mean = mean(pop))
+
+attach(iris)
+
+
+# install.packages("tidyr", dependencies = TRUE)
+
+#making library
+
+library("tidyr")
+# turn iris dataset from wide into long format
+iris.long <- gather(iris, "Measurement", "Value", 1:4)
+view(iris.long)
+
